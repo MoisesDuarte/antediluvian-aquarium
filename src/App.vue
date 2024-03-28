d832d246ca0ccbdb8c58aff806d0400d7c428238
MoisesDuarte <moises.junior@zrp.com.br>
2024-03-28 17:23:32 -0300

    chore: cleaning up unused files and adding styling

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <main>Hello World!</main>
 </template>
+
+<script setup lang="ts"></script>
 
 <style scoped>
 header {
