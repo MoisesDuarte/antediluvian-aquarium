6eab8b187bb1935100db6cb6740dd1109b685f95
MoisesDuarte <moises.junior@zrp.com.br>
2024-05-02 20:07:58 -0300

    style: wip a quick style for the recording

--- src/components/MainWindow.vue
+++ src/components/MainWindow.vue
@@ -1,11 +1,18 @@
 <template>
   <section :style="{ 'background-image': `url(${background})` }">
     <header>
-      <div class="title">
-        <i class="ri-map-pin-2-fill"></i>
-        {{ name }}
+      <div class="scene-title">
+        <i class="ri-circle-fill"></i> REC
       </div>
+
+      <i class="ri-battery-low-line"></i>
+
     </header>
+
+    <footer>
+      <i class="ri-map-pin-fill"></i>
+      {{ name }}
+    </footer>
 
     <div class="crt-noise" />
     <div class="crt-overlay" />
@@ -24,13 +31,52 @@ header {
   justify-content: space-between;
 }
 
-header>.title {
-  z-index: 2;
+footer {
+  padding: 12px 16px;
+  text-shadow: 0 0 5px var(--primary-color);
+  text-align: right;
+}
+
+header>.scene-title {
+  background: none;
+  color: var(--primary-color);
+  text-shadow: 0 0 5px var(--primary-color);
+  text-transform: uppercase;
+  font-weight: 600;
+  letter-spacing: 1px;
+  padding: 12px 16px;
+}
+
+header>.scene-title>i {
+  width: 60px;
+  animation: blink 1.25s ease-in-out infinite;
+}
+
+header>i {
+  font-size: 36px;
+  padding-right: 12px;
 }
 
 section {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   border: 1px solid var(--primary-color);
   position: relative;
   background-size: cover;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
 }
 </style>
